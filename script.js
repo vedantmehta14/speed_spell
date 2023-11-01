bb55e8c6aa1602823132c6619396be840230a188
Vedant Mehta <95914389+vedantmehta14@users.noreply.github.com>
2023-11-01 09:42:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreElement = document.getElementById("score");
+const timeElement = document.getElementById("time");
+const endgameElement = document.getElementById("end-game-container");
+const settingsButton = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+// List of words for game
+const wordsEasy = [
+  "sigh",
+  // "tense",
+  // "airplane",
+  "ball",
+  "pies",
+  // "juice",
+  // "warlike",
+  "bad",
+  "north",
+  // "dependent",
+  // "steer",
+  // "silver",
+  // "highfalutin",
+  // "superficial",
+  // "quince",
+  // "eight",
+  // "feeble",
+  // "admit",
+  "drag"
+  // "loving",
+];
+
+const wordsMedium = [
+  "tense",
+  // "airplane",
+  "juice",
+  // "warlike",
+  // "dependent",
+  "steer",
+  "silver",
+  // "highfalutin",
+  // "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "loving"
+]
+
+const wordsHard = [
+  "airplane",
+  "warlike",
+  "dependent",
+  "highfalutin",
+  "superficial"
+]
+
+let randomWord;
+let score = 0;
+let time = 10;
+// let difficulty = "medium";
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+const timeInterval = setInterval(updateTime, 1000);
+
+function getRandomWord() {
+  if (difficulty === "hard") 
+    return wordsHard[Math.floor(Math.random() * wordsHard.length)];
+  else if (difficulty === "medium") 
+    return wordsMedium[Math.floor(Math.random() * wordsMedium.length)];
+  else 
+    return wordsEasy[Math.floor(Math.random() * wordsEasy.length)];
+}
+
+function addWordToDom() { 
+  randomWord = getRandomWord();
+  word.innerText = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreElement.innerText = score;
+}
+
+function updateTime() {
+  time--;
+  timeElement.innerText = time + "s";
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function gameOver() {
+  endgameElement.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="history.go(0)">Play Again</button>
+    `;
+  endgameElement.style.display = "flex";
+}
+
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+  if (insertedText === randomWord) {
+    e.target.value = "";
+    addWordToDom();
+    updateScore();
+    if (difficulty === "hard") time += 2;
+    else if (difficulty === "medium") time += 3;
+    else time += 5;
+    updateTime();
+  }
+});
+
+settingsButton.addEventListener("click", () =>
+  settings.classList.toggle("hide")
+);
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
+
+// Initialization
+difficultySelect.value = difficulty;
+addWordToDom();
+text.focus();
